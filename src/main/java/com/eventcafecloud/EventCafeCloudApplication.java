af05202a9b229bc6688f7225e3a0f5fc18ee6552
DESKTOP-G3USBLG\simba <simba0502@naver.com>
2022-06-24 16:14:55 +0900

    init

---
+++ src/main/java/com/eventcafecloud/EventCafeCloudApplication.java
@@ -0,0 +1,13 @@
+package com.eventcafecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCafeCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventCafeCloudApplication.class, args);
+    }
+
+}
